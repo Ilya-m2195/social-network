3e3e0e7aba1b5ebf0ad4d731ea32c109380af407
Ilya Myasnichenko <ilya.m.9595@mail.ru>
2023-06-13 17:25:26 +0600

    add components

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+const Navigation = () => {
+    return (
+        <nav className='header__nav'>
+          <ul>
+            <li className='nav__item'> <a className='nav__link' href='#1'>Profile</a> </li>
+            <li className='nav__item'> <a className='nav__link' href='#2'>Messages</a> </li>
+            <li className='nav__item'> <a className='nav__link' href='#3'>News</a> </li>
+            <li className='nav__item'> <a className='nav__link' href='#4'>Music</a> </li>
+            <li className='nav__item'> <a className='nav__link' href='#5'>setting</a> </li>
+          </ul>
+        </nav>
+    )
+}
+
+export default Navigation;
+
